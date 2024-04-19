27644183ec3b3108510114ab418c5b886f838e2d
Gift <aghaulor.gift@gmail.com>
2024-04-19 06:59:07 -0700

    Add package 'flash'

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to install Flask version 2.1.0 using pip3
+# Description: Ensures Flask version 2.1.0 is installed via pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
