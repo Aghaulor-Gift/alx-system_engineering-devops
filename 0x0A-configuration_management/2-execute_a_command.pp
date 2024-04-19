654f3e685d44a5a4c2da83d897363ae3ea7f5db7
Gift <aghaulor.gift@gmail.com>
2024-04-19 12:51:51 -0700

    Execute pkill for file

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to kill a process named 'killmenow'
+
+exec { 'killmenow':
+  command     => '/usr/bin/pkill killmenow',
+  refreshonly => true,
+  onlyif => '/usr/bin/pgrep killmenow'
+}
