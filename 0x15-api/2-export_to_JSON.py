3155f61b4469933df87756d6755270ed7f6ac7bf
Gift <aghaulor.gift@gmail.com>
2024-05-22 09:36:18 -0700

    Updated script

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 """Python script that extends script from Task 0
-and exports data in JSON formatted file
-File must have all records
+and exports data in JSON formatted file.
+File must have all records.
 """
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <EMPLOYEE_ID>")
+        sys.exit(1)
+
     EMPLOYEE_ID = sys.argv[1]
+
+    try:
+        EMPLOYEE_ID = int(EMPLOYEE_ID)
+    except ValueError:
+        print("EMPLOYEE_ID must be an integer")
+        sys.exit(1)
+
     USERNAME = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{:d}"
-        .format(int(EMPLOYEE_ID))).json().get("username")
+        f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}"
+    ).json().get("username")
+
     ALL_TASKS = []
-    req = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-    for task in req:
-        if (task.get("userId") == int(EMPLOYEE_ID)):
-            dict = {}
-            dict["task"] = task.get("title")
-            dict["completed"] = task.get("completed")
-            dict["username"] = USERNAME
-            ALL_TASKS.append(list)
+    for task in tasks:
+        if task.get("userId") == EMPLOYEE_ID:
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = USERNAME
+            ALL_TASKS.append(task_dict)
 
-    with open("{}.json".format(EMPLOYEE_ID), 'w') as jsonfile:
+    with open(f"{EMPLOYEE_ID}.json", 'w') as jsonfile:
         json.dump({EMPLOYEE_ID: ALL_TASKS}, jsonfile)
